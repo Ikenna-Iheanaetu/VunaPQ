2d628d57816b1692ac43de73bb8afec9edd5b960
Ikenna Iheanaetu <ikennae120@gmail.com>
2023-05-18 17:02:00 +0100

    Added some functionality to adding files and rendering files adding to the webpage

--- src/Pages/upload.jsx
+++ src/Pages/upload.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import { useState } from "react"
 import { Link } from 'react-router-dom';
 import Image from '../assets/image3.png'
 import { IoCloudUploadOutline } from "react-icons/io5";
 import Design from '../components/design';
+import FileList from '../components/filelist';
 
 const Upload = () => {
     const height = 'h-[300px]'
+
+    const [department, setDeparment] = useState('')
+    const [semester, setSemester] = useState('')
+    const [courseCode, setCourseCode] = useState('')
+    const [files, setFiles] = useState([])
+
+    const onChangeDepartment = (event) => {
+        const value = event.value
+        setDeparment(value)
+
+    }
+    const onChangeSemester = (event) => {
+        const value = event.target.value
+        setSemester(value)
+    }
+    const onChangeCourseCode = (event) => {
+        const value = event.target.value
+        setCourseCode(value)
+    }
+    const onChangeFile = (event) => {
+        const file = event.target.files[0]
+        setFiles([...files, file])
+    }
+    console.log(files);
 
     return (
         <>
@@ -29,19 +55,19 @@ const Upload = () => {
                             <div className='col-span-6'>
                                 <div className='flex flex-col gap-3'>
                                     <label for="Department">Department</label>
-                                    <input type="text" name="" value="" placeholder='e.g Software Engineering' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
+                                    <input type="text" name="" value={department} onChange={onChangeDepartment} placeholder='e.g Software Engineering' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
                                 </div>
                             </div>
                             <div className='col-span-3'>
                                 <div className='flex flex-col gap-3'>
                                     <label for="Department">Semester</label>
-                                    <input type="text" name="" value="" placeholder='e.g Second' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
+                                    <input type="text" name="" value={semester} onChange={onChangeSemester} placeholder='e.g Second' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
                                 </div>
                             </div>
                             <div className='col-span-3'>
                                 <div className='flex flex-col gap-3'>
                                     <label for="Department">Course Code</label>
-                                    <input type="text" name="" value="" placeholder='e.g SEN182' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
+                                    <input type="text" name="" value={courseCode} onChange={onChangeCourseCode} placeholder='e.g SEN182' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
                                 </div>
                             </div>
                             <div className='col-span-6'>
@@ -49,12 +75,15 @@ const Upload = () => {
                                     <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-800 border-dashed rounded-lg cursor-pointer bg-gray-50 =">
                                         <div class="flex flex-col items-center justify-center pt-5 pb-6">
                                             <IoCloudUploadOutline className='text-3xl mb-5' />
-                                            <p class="mb-2 text-sm text-gray-900"><span class="font-semibold text-[18px]">Click to upload</span></p>
+                                            <p class="mb-2 text-sm text-gray-900"><span class="font-semibold text-[18px]">Click to Upload</span></p>
                                             <p class="text-xs text-gray-900">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                                         </div>
-                                        <input id="dropzone-file" type="file" class="hidden" />
+                                        <input id="dropzone-file" type="file" onChange={onChangeFile} class="hidden" />
                                     </label>
                                 </div>
+                            </div>
+                            <div className='col-span-6'>
+                                <FileList files={files} setFiles={setFiles} />
                             </div>
                         </div>
 
@@ -65,7 +94,6 @@ const Upload = () => {
                     </button>
                 </div>
             </div>
-
 
             <div className='min-[770px]:hidden z-[1000px]'>
                 <div className='mb-5 h-[300px] bg-gradient-to-b from-green-400 to-slate-900 to-80% rounded-b-xl flex justify-center items-center'>
@@ -79,15 +107,15 @@ const Upload = () => {
                 <div className='p-7 flex flex-col gap-4'>
                     <div className='flex flex-col gap-3'>
                         <label for="Department">Department</label>
-                        <input type="text" name="" value="" placeholder='e.g Software Engineering' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
+                        <input type="text" name="" value={department} onChange={onChangeDepartment} placeholder='e.g Software Engineering' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
                     </div>
                     <div className='flex flex-col gap-3'>
                         <label for="Department">Semester</label>
-                        <input type="text" name="" value="" placeholder='e.g Second' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
+                        <input type="text" name="" value={semester} onChange={onChangeSemester} placeholder='e.g Second' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
                     </div>
                     <div className='flex flex-col gap-3'>
                         <label for="Department">Course Code</label>
-                        <input type="text" name="" value="" placeholder='e.g SEN182' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
+                        <input type="text" name="" value={courseCode} onChange={onChangeCourseCode} placeholder='e.g SEN182' className='border-[1px] border-black outline-none h-[40px] rounded-md p-6' />
                     </div>
                     <div class="flex items-center justify-center w-full">
                         <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-800 border-dashed rounded-lg cursor-pointer bg-gray-50 =">
@@ -96,8 +124,11 @@ const Upload = () => {
                                 <p class="mb-2 text-sm text-gray-900"><span class="font-semibold text-[18px]">Click to upload</span></p>
                                 <p class="text-xs text-gray-900">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                             </div>
-                            <input id="dropzone-file" type="file" class="hidden" />
+                            <input id="dropzone-file" type="file" onChange={onChangeFile} class="hidden" />
                         </label>
+                    </div>
+                    <div className='col-span-6'>
+                        <FileList files={files} setFiles={setFiles} />
                     </div>
                     <button className='flex justify-center mt-9 gap-2 border-[1px] border-gray-900 p-4 rounded-md hover:bg-black hover:first:text-white hover:last:text-white'>
                         <IoCloudUploadOutline className='text-2xl' />
@@ -106,16 +137,12 @@ const Upload = () => {
                 </div>
             </div>
 
-
-
             <div className="z-[-10px] min-[770px]:hidden w-full absolute top-0 left-0">
                 <Design height={height} />
             </div>
 
-
         </>
-
     )
 }
 
-export default Upload 
+export default Upload
---
+++ src/components/filelist.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { HiOutlineTrash } from "react-icons/hi2";
+
+const FileList = ({ files, setFiles }) => {
+    const filterFiles = (filename) => {
+        setFiles(files.filter(file => file.name !== filename))
+    }
+
+    return (
+        <>
+            <ul className='flex flex-col gap-4'>
+                {files && files.map( file => (
+                    <div className='flex justify-between  items-center p-4 rounded-md border-[1px] border-black' key={file.name}>
+                        <li className='text-ellipsis overflow-hidden '>{file.name}</li>
+                        <HiOutlineTrash className='text-3xl hover:cursor-pointer hover:border-[1px] hover:border-black rounded-[4px] p-1' onClick={() => filterFiles(file.name)} />
+                    </div>
+                ))}
+            </ul>
+        </>
+    )
+}
+
+export default FileList
