08a851ac54d050b5ac70caf82fd996f6218f118a
Ikenna Iheanaetu <ikennae120@gmail.com>
2023-05-18 17:37:18 +0100

    Adjusted the file list style

--- src/components/filelist.jsx
+++ src/components/filelist.jsx
@@ -11,8 +11,8 @@ const FileList = ({ files, setFiles }) => {
             <ul className='flex flex-col gap-4'>
                 {files && files.map( file => (
                     <div className='flex justify-between  items-center p-4 rounded-md border-[1px] border-black' key={file.name}>
-                        <li className='text-ellipsis overflow-hidden'>{file.name}</li>
-                        <HiOutlineTrash className='text-3xl hover:cursor-pointer hover:border-[1px] hover:border-black rounded-[4px] p-1' onClick={() => filterFiles(file.name)} />
+                        <li className='max-[10px]:text-ellipsis overflow-hidden'>{file.name}</li>
+                        <HiOutlineTrash className='text-3xl max-[500px]:text-4xl hover:cursor-pointer hover:border-[1px] hover:border-black rounded-[4px] p-1' onClick={() => filterFiles(file.name)} />
                     </div>
                 ))}
             </ul>
