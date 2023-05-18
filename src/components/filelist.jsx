7e76a4a8917bbb9c504b2176212090d78297872d
Ikenna Iheanaetu <ikennae120@gmail.com>
2023-05-18 19:21:54 +0100

    Added style and functionality to check if a file has been chosen

--- src/components/filelist.jsx
+++ src/components/filelist.jsx
@@ -10,7 +10,7 @@ const FileList = ({ files, setFiles }) => {
     return (
         <>
             <ul className='flex flex-col gap-4'>
-                <h3  className={files.length > 0 ? 'text-green-500' : 'text-red-600'}>{files.length > 0 ? 'Chosen File(s)' : 'No Chosen Files'}</h3>
+                <h3  className={files.length > 0 ? 'text-green-500' : 'text-red-600'}>{files.length > 0 ? 'Chosen File(s)' : 'No Chosen File(s)'}</h3>
                 {files && files.map( file => (
                     <div className='flex justify-between  items-center p-4 rounded-md border-[1px] border-black' key={file.name}>
                         <li className='inline-block whitespace-nowrap text-ellipsis overflow-hidden pl-2'>{file.name}</li>
